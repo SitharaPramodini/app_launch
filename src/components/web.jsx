5744e734a6d36e62c9ae9636e08c00a47f8bac69
SitharaPramodini <it21306754@my.sliit.lk>
2025-03-14 11:20:49 +0530

    video

--- src/components/web.jsx
+++ src/components/web.jsx
@@ -36,7 +36,7 @@ const Web = () => {
               muted
               onEnded={handleVideoEnd}
             >
-              <source src="title-video.mp4" type="video/mp4" />
+              <source src="title-last1.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           ) : (
